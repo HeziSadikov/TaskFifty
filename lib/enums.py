720db9e61d3b16673402b23efd672e85f9ebe7af
Hezi Sadikov <hezisadikov@gmail.com>
2023-03-23 22:39:15 +0200

    Added Column Enum

--- lib/enums.py
+++ lib/enums.py
@@ -10,3 +10,11 @@ class Priority(Enum):
 class Status(Enum):
     TODO = 1
     DONE = 2
+
+
+class Column(Enum):
+    TITLE = 1
+    DESCRIPTION = 2
+    DEADLINE = 3
+    PRIORITY = 4
+    STATUS = 5
