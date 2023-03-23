9ac6f75085e270c20f6b87808c81594353909fcc
Hezi Sadikov <hezisadikov@gmail.com>
2023-03-23 22:36:14 +0200

    delete_task and update_task improvements

--- services/task_service.py
+++ services/task_service.py
@@ -1,32 +1,33 @@
 from prettytable import from_db_cursor
 from services.db_service import db_instance
 from lib.utils import get_prompt, update_status_if_late
-from lib.enums import Priority, Status
+from lib.enums import Priority, Status, Column
 
 
 def view_tasks():
     update_status_if_late()
     db_instance().cursor.execute(
-        """SELECT
-                id,
-                title,
-                description,
-                strftime('%H:%M, %d-%m-%Y', deadline, 'unixepoch', 'localtime') as deadline,
-            CASE priority
-                WHEN 1 THEN 'LOW'
-                WHEN 2 THEN 'MEDIUM'
-                WHEN 3 THEN 'HIGH'
-                ELSE 'UNKNOWN'
-            END AS priority,
-            CASE status
-                WHEN 1 THEN 'TODO'
-                WHEN 2 THEN 'DONE'
-                WHEN 3 THEN 'LATE'
-                ELSE 'UNKNOWN'
-            END AS status,
-                strftime('%H:%M:%S, %d-%m-%Y', created, 'unixepoch', 'localtime') as created,
-                strftime('%H:%M:%S, %d-%m-%Y', updated, 'unixepoch', 'localtime') as updated
-            FROM tasks"""
+        """
+        SELECT
+            id,
+            title,
+            description,
+        strftime('%H:%M, %d-%m-%Y', deadline, 'unixepoch', 'localtime') as deadline,
+        CASE priority
+            WHEN 1 THEN 'LOW'
+            WHEN 2 THEN 'MEDIUM'
+            WHEN 3 THEN 'HIGH'
+            ELSE 'UNKNOWN'
+        END AS priority,
+        CASE status
+            WHEN 1 THEN 'TODO'
+            WHEN 2 THEN 'DONE'
+            WHEN 3 THEN 'LATE'
+            ELSE 'UNKNOWN'
+        END AS status,
+        strftime('%H:%M:%S, %d-%m-%Y', created, 'unixepoch', 'localtime') as created,
+        strftime('%H:%M:%S, %d-%m-%Y', updated, 'unixepoch', 'localtime') as updated
+        FROM tasks"""
     )
     pretty_formatted_table = from_db_cursor(db_instance().cursor)
     print("\n", pretty_formatted_table, sep="")
@@ -62,45 +63,42 @@ def add_task():
 
 
 def delete_task():
-    while True:
-        view_tasks()
-        id_to_delete = input(
-            "\nPlease type the id of the task you wish to delete: "
-        ).strip()
-        try:
-            db_instance().cursor.execute("DELETE FROM tasks WHERE id = ?", id_to_delete)
-            return
-        except:
-            db_instance().conn.rollback()
+    view_tasks()
+    id_to_delete = get_prompt(
+        "task id", "\nPlease type the id of the task you wish to delete: "
+    )
+    db_instance().cursor.execute("DELETE FROM tasks WHERE id = ?", str(id_to_delete))
 
 
 def update_task():
-    allowed_columns = ["title", "description", "deadline", "priority", "status"]
-    while True:
-        view_tasks()
+    view_tasks()
 
-        id_to_update = get_prompt(
-            "task id", "\nPlease type the id of the task you wish to update: "
+    id_to_update = get_prompt(
+        "task id", "\nPlease type the id of the task you wish to update: "
+    )
+
+    chosen_column = get_prompt(
+        "column",
+        "\nPlease choose a column:\n\n"
+        + "\n".join(f"{c.value}. {c.name.title()}" for c in Column)
+        + "\n\nYour choice: ",
+    )
+
+    chosen_column_name = Column(chosen_column).name.lower()
+
+    updated_column = get_prompt(
+        chosen_column_name, f"\nEnter the new {chosen_column_name}: "
+    )
+
+    try:
+        db_instance().cursor.execute(
+            f"""UPDATE tasks
+                SET {chosen_column_name} = ?, updated = strftime('%s', 'now')
+                WHERE id = ?;""",
+            (updated_column, id_to_update),
         )
+        return
 
-        while True:
-            column = input("In which column? ")
-            if column not in allowed_columns:
-                print("Invalid column, please try again.")
-                continue
-            break
-
-        updated_column = get_prompt(column, f"Enter the new {column}: ")
-
-        try:
-            db_instance().cursor.execute(
-                f"""UPDATE tasks
-                    SET {column} = ?, updated = strftime('%s', 'now')
-                    WHERE id = ?;""",
-                (updated_column, id_to_update),
-            )
-            return
-
-        except:
-            print("ERROR")
-            db_instance().conn.rollback()
+    except:
+        print("ERROR")
+        db_instance().conn.rollback()
