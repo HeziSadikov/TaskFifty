b8d708b2ce39a475abca0d0fd4598ba551cbffd8
Hezi Sadikov <hezisadikov@gmail.com>
2023-03-23 14:16:22 +0200

    split valid_prompt into fn's for readability

--- lib/utils.py
+++ lib/utils.py
@@ -7,7 +7,7 @@ from lib.enums import Priority, Status
 def get_prompt(field, prompt):
     while True:
         user_input = input(prompt).strip()
-        valid_input = valid_prompt(field, user_input)
+        valid_input = validate_prompt(field, user_input)
 
         if not user_input:
             print(f"\nThe {field} can't be empty!\n")
@@ -18,44 +18,55 @@ def get_prompt(field, prompt):
             return valid_input
 
 
-def valid_prompt(field, user_input):
+def validate_prompt(field, user_input):
     if field in ["title", "description", "answer"]:
         return user_input
 
     elif field == "deadline":
-        today = datetime.now()
-        local_timezone = dateutil.tz.tzlocal()
-        try:
-            input_datetime = datetime.strptime(user_input, "%H:%M %d-%m-%y")
-
-            if today > input_datetime:
-                print("\nThe deadline must be in the future!")
-                return None
-
-            input_datetime = input_datetime.replace(tzinfo=local_timezone)
-            utc_datetime_object = input_datetime.astimezone(pytz.utc)
-
-            return utc_datetime_object.strftime("%Y-%m-%d %H:%M:%S")
-
-        except ValueError:
-            print("\nInvalid format, please try again.")
-            return None
+        return validate_deadline(user_input)
 
     elif field == "priority":
-        user_input = user_input.upper()
-        hashed_input = Priority[user_input].value
-        if hashed_input not in [0, 1, 2]:
-            print(
-                "\nPriority can only be: High, Medium or Low.\n" + "Please try again."
-            )
-            return None
-        else:
-            return hashed_input
+        return validate_enum(user_input, Priority)
 
     elif field == "status":
-        user_input = user_input.upper()
-        hashed_input = Status[user_input].value
-        if hashed_input not in ["TODO", "DONE"]:
-            print("\nYou can only choose to mark a task as DONE/TODO\n")
+        return validate_enum(user_input, Status)
+
+    else:
+        print(f"{field} is not a valid field.")
+        return None
+
+
+def validate_deadline(user_input):
+    today = datetime.now()
+    local_timezone = dateutil.tz.tzlocal()
+
+    try:
+        input_datetime = datetime.strptime(user_input, "%H:%M %d-%m-%y")
+
+        if today > input_datetime:
+            print("\nThe deadline must be in the future!")
             return None
-        return hashed_input
+
+        input_datetime = input_datetime.replace(tzinfo=local_timezone)
+        utc_datetime = input_datetime.astimezone(pytz.utc)
+
+        return utc_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+    except ValueError:
+        print("\nInvalid format, please try again.")
+        return None
+
+
+def validate_enum(user_input, input_enum):
+    try:
+        return input_enum(int(user_input)).value
+    except ValueError:
+        pass
+
+    try:
+        return input_enum[user_input.upper()].value
+    except KeyError:
+        pass
+
+    print("\nInvalid choice.")
+    return None
